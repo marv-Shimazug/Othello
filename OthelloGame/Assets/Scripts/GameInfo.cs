863e1232cbafd6659b0fe6e68363f35439cfca4b
shimazug <shimazug@marv.jp>
2015-09-28 12:13:17 +0900

    add １手ごとの制限時間

--- OthelloGame/Assets/Scripts/GameInfo.cs
+++ OthelloGame/Assets/Scripts/GameInfo.cs
@@ -1,17 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameInfo : MonoBehaviour {
 
 	private static bool m_BlackPlayer;	// true:プレイヤー	false:AI.
 	private static bool m_WhitePlayer;	// true:プレイヤー	false:AI
-	private  int m_BoardNumButton;		// ボードの数.
+
 	private static int m_Row;
 	private static int m_Column;
+	private static int m_TimeLimit;		// 一手ごとの制限時間/秒.
+
+	private  int m_BoardNumButton;	// ボードの数設定ボタン.
+	private int m_TimeLimitButton;		// タイムリミット設定ボタン.
+	[SerializeField]Text m_TimeLimitText;
 
 	// アクセサ.
 	public bool BlackPlayer{get{return m_BlackPlayer;}set{m_BlackPlayer = value;}}
 	public bool WhitePlayer{get{return m_WhitePlayer;}set{m_WhitePlayer = value;}}
+
+	void Start()
+	{
+		m_TimeLimitText.text = "なし";
+	}
 
 	// プレイヤーが何人存在するかを返却.
 	public int PlayerNum(){
@@ -29,6 +40,7 @@ public class GameInfo : MonoBehaviour {
 	}
 
 
+	// 盤面のマスの数.
 	public void OnClickBoardNum()
 	{
 		switch (m_BoardNumButton) 
@@ -51,6 +63,45 @@ public class GameInfo : MonoBehaviour {
 		case 3:
 			m_Row = 10;
 			m_Column = 10;
+			break;
+		}
+	}
+
+
+	// 制限時間の設定.
+	public void OnTimeLimit()
+	{
+		m_TimeLimitButton++;
+		if (m_TimeLimitButton > 4) 
+		{
+			m_TimeLimitButton = 0;
+		}
+
+		switch(m_TimeLimitButton)
+		{
+		case 0:
+			m_TimeLimit = 0;
+			m_TimeLimitText.text = "なし";
+			break;
+
+		case 1:
+			m_TimeLimit = 3;
+			m_TimeLimitText.text = ((int)m_TimeLimit).ToString() + "秒";
+			break;
+		
+		case 2:
+			m_TimeLimit = 5;
+			m_TimeLimitText.text = ((int)m_TimeLimit).ToString() + "秒";
+			break;
+		
+		case 3:
+			m_TimeLimit = 10;
+			m_TimeLimitText.text = ((int)m_TimeLimit).ToString() + "秒";
+			break;
+
+		case 4:
+			m_TimeLimit = 30;
+			m_TimeLimitText.text = ((int)m_TimeLimit).ToString() + "秒";
 			break;
 		}
 	}
---
+++ OthelloGame/Assets/Scripts/TimeLimit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeLimit : MonoBehaviour {
+
+	private float time = 3;
+	private Board board;			// ボードにアクセスするための情報格納庫.
+
+	void Start()
+	{
+		board = GameObject.Find ("Board").GetComponent<Board> ();
+		GetComponent<Text> ().text = ((int)time).ToString ();
+	}
+
+	void Update()
+	{
+		// 決着までの時間をカウント.
+		if (board.SetAvailableManager) 
+		{
+			time -= Time.deltaTime;
+			GetComponent<Text> ().text = ((int)time).ToString ();
+		}
+	}
+}
